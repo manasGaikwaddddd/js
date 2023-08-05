5c1b736dd54038de4aab10efa2bf270fd35f846e
manasGaikwaddddd <98682699+manasGaikwaddddd@users.noreply.github.com>
2023-08-05 22:23:48 +0530

    Add files via upload

---
+++ lovecalculator.py
@@ -0,0 +1,5 @@
+print ('welcome to love calculator')
+name1= int(input("what is your name"/n))
+name2= int(input('what is their name'/n))
+combined_string = name1+name2
+lowercasestring = combined_string.lower()
